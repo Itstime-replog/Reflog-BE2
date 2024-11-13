3ccc41ea5f58c2153e2940c101f770610f36da98
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-13 10:30:39 +0900

    feat: init project

---
+++ src/main/java/itstime/reflog/ReflogApplication.java
@@ -0,0 +1,13 @@
+package itstime.reflog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itstime/reflog/ReflogApplicationTests.java
@@ -0,0 +1,13 @@
+package itstime.reflog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
