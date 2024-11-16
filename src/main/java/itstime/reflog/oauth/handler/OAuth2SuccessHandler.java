d9bca7fc19336588462cc037a31adbdc0fd698af
Jeongdonghun <arrrr180@konkuk.ac.kr>
2024-11-16 14:22:20 +0900

    feat : Jwt token 설정 #8

--- src/main/java/itstime/reflog/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/itstime/reflog/oauth/handler/OAuth2SuccessHandler.java
@@ -2,6 +2,8 @@ package itstime.reflog.oauth.handler;
 
 import java.io.IOException;
 
+import itstime.reflog.oauth.domain.UserPrincipal;
+import itstime.reflog.oauth.token.JwtTokenProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -14,10 +16,19 @@ import lombok.RequiredArgsConstructor;
 @Component
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-		Authentication authentication) throws IOException{
+    private final JwtTokenProvider jwtTokenProvider;
 
-	}
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
 
+        // Generate JWT
+        String jwtToken = jwtTokenProvider.createToken(userPrincipal.getMember().getId().toString());
+
+        response.addHeader("Authorization", "Bearer " + jwtToken);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"token\": \"" + jwtToken + "\"}");
+    }
 }
