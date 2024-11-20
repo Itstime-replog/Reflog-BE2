acb8b15fbf5de29341f85cf621da76a0e471face
jeong724 <oliveyou724@gmail.com>
2024-11-20 13:11:32 +0900

    feat : 스웨거에 jwt를 추가한다 #7
    - SwaggerConfig jwt 설정 추가
    fixes : #7

--- src/main/java/itstime/reflog/config/SwaggerConfig.java
+++ src/main/java/itstime/reflog/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package itstime.reflog.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +13,23 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI SwaggerAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .scheme("bearer")
+                .bearerFormat("JWT");
 
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
+                .info(apiInfo());
     }
 
-    private Info apiInfo(){
+    private Info apiInfo() {
         return new Info()
                 .title("Reflog Swagger")
                 .description("Reflog Server API 명세서")
