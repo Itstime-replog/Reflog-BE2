3c13cf55baf87ae92775c200dde7df3cbbd11508
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-25 22:36:38 +0900

    feat: validation 추가 및 투두리스트 DTO 생성 #11

---
+++ src/main/java/itstime/reflog/todolist/dto/TodolistDTO.java
@@ -0,0 +1,34 @@
+package itstime.reflog.todolist.dto;
+
+import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class TodolistDTO {
+
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PRIVATE)
+	public static class TodolistSaveOrUpdateRequest {
+
+		@NotBlank(message = "content는 비어 있을 수 없습니다.")
+		private String content;
+
+		@NotNull(message = "status는 null일 수 없습니다.")
+		@AssertFalse(message = "status는 초기 상태에서 false여야 합니다.")
+		private boolean status;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	public static class TodolistResponse {
+		private final Long todolistId;
+		private final String content;
+		private final boolean status;
+	}
+
+
+}
