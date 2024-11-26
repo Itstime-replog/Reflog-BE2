5b55c6e282e3756d9cc48ed6611081d7d992a0a3
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-11-26 19:48:36 +0900

    chore: 멤버와 todolist 연결 #11

--- src/main/java/itstime/reflog/member/domain/Member.java
+++ src/main/java/itstime/reflog/member/domain/Member.java
@@ -1,5 +1,9 @@
 package itstime.reflog.member.domain;
 
+import java.util.List;
+
+import itstime.reflog.todolist.domain.Todolist;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,6 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,5 +37,9 @@ public class Member {
 	private String name;
 
 	private String profileImageUrl;
+
+	@OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+	private List<Todolist> todolists;
+
 
 }
