b2c8fe0a0ba3b93be9e59cc970803b85113c891c
Jeongdonghun <arrrr180@konkuk.ac.kr>
2024-11-28 22:18:25 +0900

    feat : 소셜로그인 handler 수정 #8

---
+++ src/main/java/itstime/reflog/oauth/handler/OAuthLoginSuccessHandler.java
@@ -0,0 +1,112 @@
+package itstime.reflog.oauth.handler;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Map;
+import java.util.UUID;
+
+import itstime.reflog.member.domain.Member;
+import itstime.reflog.member.repository.MemberRepository;
+import itstime.reflog.oauth.info.OAuth2UserInfo;
+import itstime.reflog.oauth.info.KakaoUserInfo;
+import itstime.reflog.oauth.info.NaverUserInfo;
+import itstime.reflog.oauth.token.domain.RefreshToken;
+import itstime.reflog.oauth.token.repository.RefreshTokenRepository;
+import itstime.reflog.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuthLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Value("${jwt.redirect}")
+    private String REDIRECT_URI; // 프론트엔드로 Jwt 토큰을 리다이렉트할 URI
+
+    @Value("${jwt.access-token.expiration-time}")
+    private long ACCESS_TOKEN_EXPIRATION_TIME; // 액세스 토큰 유효기간
+
+    @Value("${jwt.refresh-token.expiration-time}")
+    private long REFRESH_TOKEN_EXPIRATION_TIME; // 리프레쉬 토큰 유효기간
+
+    private OAuth2UserInfo oAuth2UserInfo = null;
+
+    private final JwtUtil jwtUtil;
+    private final MemberRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication; // 토큰
+        final String provider = token.getAuthorizedClientRegistrationId(); // provider 추출
+
+        // 구글 || 카카오 || 네이버 로그인 요청
+        switch (provider) {
+            case "kakao" -> {
+                log.info("카카오 로그인 요청");
+                oAuth2UserInfo = new KakaoUserInfo(token.getPrincipal().getAttributes());
+            }
+            case "naver" -> {
+                log.info("네이버 로그인 요청");
+                oAuth2UserInfo = new NaverUserInfo((Map<String, Object>) token.getPrincipal().getAttributes().get("response"));
+            }
+        }
+
+        // 정보 추출
+        String providerId = oAuth2UserInfo.getProviderId();
+        String name = oAuth2UserInfo.getName();
+        String profileImageUrl = oAuth2UserInfo.getProfileImageUrl();
+
+        Member existUser = userRepository.findByProviderId(providerId);
+        Member user;
+
+        if (existUser == null) {
+            // 신규 유저인 경우
+            log.info("신규 유저입니다. 등록을 진행합니다.");
+
+            user = Member.builder()
+                    .memberId(UUID.randomUUID())
+                    .name(name)
+                    .provider(provider)
+                    .providerId(providerId)
+                    .profileImageUrl(profileImageUrl)
+                    .build();
+            userRepository.save(user);
+        } else {
+            // 기존 유저인 경우
+            log.info("기존 유저입니다.");
+            refreshTokenRepository.deleteByMemberId(existUser.getMemberId());
+            user = existUser;
+        }
+
+        log.info("유저 이름 : {}", name);
+        log.info("PROVIDER : {}", provider);
+        log.info("PROVIDER_ID : {}", providerId);
+
+        // 리프레쉬 토큰 발급 후 저장
+        String refreshToken = jwtUtil.generateRefreshToken(user.getMemberId(), REFRESH_TOKEN_EXPIRATION_TIME);
+
+        RefreshToken newRefreshToken = RefreshToken.builder()
+                .memberId(user.getMemberId())
+                .refreshToken(refreshToken)
+                .build();
+        refreshTokenRepository.save(newRefreshToken);
+
+        // 액세스 토큰 발급
+        String accessToken = jwtUtil.generateAccessToken(user.getMemberId(), ACCESS_TOKEN_EXPIRATION_TIME);
+
+        // 이름, 액세스 토큰, 리프레쉬 토큰을 담아 리다이렉트
+        String encodedName = URLEncoder.encode(name, "UTF-8");
+        String redirectUri = String.format(REDIRECT_URI, encodedName, accessToken, refreshToken);
+        getRedirectStrategy().sendRedirect(request, response, redirectUri);
+    }
+}
+
