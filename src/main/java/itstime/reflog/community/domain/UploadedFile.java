98016aca27d99e7c1b543ef0047051082831f329
jihyeon baek <qormoon@sookmyung.ac.kr>
2024-12-29 02:20:58 +0900

    feat: UploadFile 엔티티 생성 #29

---
+++ src/main/java/itstime/reflog/community/domain/UploadedFile.java
@@ -0,0 +1,33 @@
+package itstime.reflog.community.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UploadedFile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String fileName; // 원본 파일 이름
+	private String fileType; // 파일 유형 (예: PDF, IMAGE)
+	private String fileUrl;  // S3 URL
+
+	@ManyToOne
+	@JoinColumn(name = "community_id")
+	private Community community; // 해당 게시글
+
+}
