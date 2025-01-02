5b896036b4bb5e3078d975787856da2efd097d35
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-01-02 18:09:33 +0900

    fix : redis 관련 repository 수정 #34

--- src/main/java/itstime/reflog/mypage/repository/MyPageRepository.java
+++ src/main/java/itstime/reflog/mypage/repository/MyPageRepository.java
@@ -3,11 +3,9 @@ package itstime.reflog.mypage.repository;
 import itstime.reflog.member.domain.Member;
 import itstime.reflog.mypage.domain.MyPage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface MyPageRepository extends JpaRepository<MyPage, Long> {
     Optional<MyPage> findByMember(Member member);
 }
