fd276ab55a10d4f85408474930a0b4b1d6460dd3
jeong724 <oliveyou724@gmail.com>
2025-01-05 15:14:46 +0900

    refactor : 게시물 좋아요 Controller 제거 -> CommunityController로 옮김#40

--- src/main/java/itstime/reflog/community/controller/CommunityController.java
+++ src/main/java/itstime/reflog/community/controller/CommunityController.java
@@ -3,6 +3,8 @@ package itstime.reflog.community.controller;
 import java.util.List;
 import java.util.UUID;
 
+import itstime.reflog.postlike.dto.PostLikeDto;
+import itstime.reflog.postlike.service.PostLikeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +33,7 @@ import lombok.RequiredArgsConstructor;
 public class CommunityController {
 	private final CommunityService communityService;
 	private final AmazonS3Manager amazonS3Manager;
+	private final PostLikeService postLikeService;
 
 	@Operation(
 		summary = "커뮤니티 파일 임시 생성 API",
@@ -161,6 +164,34 @@ public class CommunityController {
 			){
 		List<CommunityDto.CombinedCategoryResponse> responses = communityService.getFilteredCommunity(memberId,postTypes, learningTypes);
 		return ResponseEntity.ok(CommonApiResponse.onSuccess(responses));
+	}
+
+	@Operation(
+			summary = "커뮤니티 게시물 좋아요 API",
+			description = "커뮤니티 게시글 좋아요 버튼 누를때 사용하는 API입니다. 현재 버튼이 좋아요가 아닌 상태=false, 좋아요 버튼을 누르면 true를 보내주면 됩니다.",
+			responses = {
+					@ApiResponse(
+							responseCode = "200",
+							description = "커뮤니티 게시글 좋아요 성공"
+					),
+					@ApiResponse(
+							responseCode = "404",
+							description = "해당 회원을 찾을 수 없음"
+					),
+					@ApiResponse(
+							responseCode = "500",
+							description = "서버 에러"
+					)
+			}
+	)
+	@PatchMapping("/like")
+	public ResponseEntity<CommonApiResponse<Void>> updatePostLike(
+			@RequestParam Long memberId,
+			@RequestParam Long communityId,
+			@RequestBody PostLikeDto.PostLikeSaveOrUpdateRequest dto
+	){
+		postLikeService.updatePostLike(memberId, communityId, dto);
+		return ResponseEntity.ok(CommonApiResponse.onSuccess(null));
 	}
 
 
